734ededd2cd716e2f0757cfc24203495563735ce
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2009-10-08 23:39:34 -0300

    using a graph as datastructure

--- clojure_dependencies.clj
+++ clojure_dependencies.clj
@@ -1,6 +1,24 @@
 (ns clojure-graph
   (:import java.io.File)
-  (:use clojure.contrib.find-namespaces))
+  (:use [clojure.set :only (union)])
+  (:use (clojure.contrib find-namespaces graph)))
+
+
+(defn new-graph [nodes neighbors] (struct directed-graph nodes neighbors))
+
+(def empty-graph (new-graph #{} {}))
+
+(defn add-edge [graph from to]
+  (let [nodes (:nodes graph)
+        neighbors (:neighbors graph)]
+    (new-graph
+      (conj nodes from to)
+      (merge-with union neighbors {from #{to}}))))
+
+(defn add-fan [graph from & to]
+  (reduce #(add-edge %1 from %2) graph to))
+
+
 
 (defn is-dependency? [form]
   (and (list? form) (#{:require :use} (first form))))
@@ -13,8 +31,8 @@
 
 (defn libspec-dependency [spec]
   (cond
-    (symbol? spec) spec
-    (vector? spec) (first spec)
+    (symbol? spec) (str spec)
+    (vector? spec) (str (first spec))
     :else "###spec error###"))
 
 (defn prefix-list-dependencies [spec]
@@ -31,15 +49,15 @@
 
 (defn extract-dependencies [form]
   (when-let [dep-type (and (is-dependency? form) (first form))]
-    {dep-type (mapcat spec-dependencies (next form))}))
+    (mapcat spec-dependencies (next form))))
 
 
-(defn process-ns [form]
+(defn process-ns [graph form]
   (let [[ns name & forms] form]
-    {name (apply merge-with conj {} (map extract-dependencies forms))}))
+    (reduce #(apply add-fan %1 (str name) (extract-dependencies %2)) graph forms)))
 
 
 (defn dir-dependencies
   [file]
-  (map process-ns (find-ns-decls-in-dir file)))
+  (reduce process-ns empty-graph (find-ns-decls-in-dir file)))
 
