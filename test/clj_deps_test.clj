4632da851de755aec443276f8c2a557a70138a92
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2009-10-13 21:19:52 -0300

    testing write-dependency-graph

--- test/clj_deps_test.clj
+++ test/clj_deps_test.clj
@@ -1,11 +1,12 @@
 (ns test.clj-deps-test
   (:import java.io.File)
-  (:use (clojure test))
+  (:use clojure.test [clj-deps.dot :only (graph-to-dot)])
   (:require
      [clj-deps :as m]
      [test.clj-deps.graph-test :as gt]))
 
 (def source-tree-dir (File. (.. (File. *file*) (getParent)) "resources"))
+(def source-tree-dir-name (.getPath source-tree-dir))
 
 (def dep-map (m/dir-dependency-map source-tree-dir))
 
@@ -28,9 +29,13 @@
   (gt/graph-has-edges dep-graph
     'test.resources.a 'test.resources.b
     'test.resources.a 'test.resources.dir1.a
-
     'test.resources.b 'test.resources.dir2.a
     'test.resources.b 'test.resources.dir2.b
-
     'test.resources.dir1.a 'test.resources.dir1.b
     'test.resources.dir2.b 'test.resources.dir1.b))
+
+(deftest write-dependency-graph
+  (let [file (File/createTempFile "clj-deps-test" "dot")]
+    (.deleteOnExit file)
+    (m/write-dependency-graph source-tree-dir-name file)
+    (is (= (graph-to-dot dep-graph) (slurp (.getPath file))))))
