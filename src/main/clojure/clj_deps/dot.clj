7e846a78649f0d1e6ef25262dfd7bd617dcb34a6
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2009-10-21 00:10:14 -0200

    added node attributes to dot output

--- src/main/clojure/clj_deps/dot.clj
+++ src/main/clojure/clj_deps/dot.clj
@@ -1,26 +1,46 @@
 (ns clj-deps.dot
   (:use
      (clojure.contrib [graph :only (get-neighbors)]
-                      [str-utils2 :only (join)])))
+                      [str-utils2 :only (join)]
+                      [java-utils :only (as-str)])
+     [clj-deps.graph :only (get-node-data)]))
 
-(defn- escape [name] (.replace (str name) "\"" "\\\""))
 
-(defn edge-repr [from to]
-  (str "\"" (escape from) "\" -> \"" (escape to) "\""))
+(defn- escape [name] (.replace (as-str name) "\"" "\\\""))
+(defn- format-atts
+  [atts]
+  (join ", " (map #(format "\"%s\"=\"%s\"" (escape (first %)) (escape (second %))) atts)))
 
-(defn- adj-list [graph]
+(defn edge-repr
+  [from to]
+  (format "\"%s\" -> \"%s\"" (escape from) (escape to)))
+
+(defn node-repr
+  [id atts]
+  (format "\"%s\" [%s]" (escape id) (format-atts atts)))
+
+(defn- adj-list
+  [graph]
   (letfn [(node-adj-list
             [node]
             (map vector (repeat node) (get-neighbors graph node)))]
     (mapcat node-adj-list (sort (:nodes graph)))))
 
-(defn dot-graph-edges [graph]
+(defn dot-graph-edges
+  [graph]
   (let [al (adj-list graph)]
-    (join "\n" (map #(apply edge-repr %) al))))
+    (join ";\n" (map #(apply edge-repr %) al))))
 
-(defn graph-to-dot [graph]
+(defn dot-graph-nodes
+  [graph]
+  (join ";\n" (map #(node-repr %1 (get-node-data graph %1)) (sort (:nodes graph)))))
+
+(defn graph-to-dot
+  [graph]
   (str
     "digraph G {\n"
+    (dot-graph-nodes graph)
+    ";\n"
     (dot-graph-edges graph)
     "\n}\n"))
 
--- src/test/clojure/clj_deps/dot_test.clj
+++ src/test/clojure/clj_deps/dot_test.clj
@@ -11,6 +11,9 @@
   (is (= "\"hi\" -> \"bye\"" (d/edge-repr "hi" "bye")))
   (is (= "\"quoted \\\"works\\\"\" -> \"yes\"" (d/edge-repr "quoted \"works\"" "yes"))))
 
+(deftest test-node-repr
+  (is (= "\"a\" [\"label\"=\"foo\", \"color\"=\"red\"]" (d/node-repr :a {:label :foo :color :red}))))
+
 (def eg g/empty-graph)
 
 (let [g12   (g/add-edge eg 1 2)
@@ -20,10 +23,13 @@
 
   (deftest test-dot-graph-edges
     (is (= "\"1\" -> \"2\"" (d/dot-graph-edges g12)))
-    (is (= "\"1\" -> \"2\"\n\"2\" -> \"3\"" (d/dot-graph-edges g123)))
-    (is (= "\"1\" -> \"2\"\n\"2\" -> \"3\"\n\"3\" -> \"1\"" (d/dot-graph-edges g1231))))
+    (is (= "\"1\" -> \"2\";\n\"2\" -> \"3\"" (d/dot-graph-edges g123)))
+    (is (= "\"1\" -> \"2\";\n\"2\" -> \"3\";\n\"3\" -> \"1\"" (d/dot-graph-edges g1231))))
 
-  (deftest test-graph-to-dot
-    (is (= (str "digraph G {\n" (d/dot-graph-edges g12) "\n}\n") (d/graph-to-dot g12)))
-    (is (= (str "digraph G {\n" (d/dot-graph-edges g123) "\n}\n") (d/graph-to-dot g123)))
-    (is (= (str "digraph G {\n" (d/dot-graph-edges g1231) "\n}\n") (d/graph-to-dot g1231)))))
+  (let [g (-> g12 (g/add-node-data 1 {:label :one}) (g/add-node-data 2 {:label :two}))]
+    (deftest test-dot-graph-nodes
+        (is (= "\"1\" [\"label\"=\"one\"];\n\"2\" [\"label\"=\"two\"]" (d/dot-graph-nodes g))))
+
+    (deftest test-graph-to-dot
+      (is (= (str "digraph G {\n" (d/dot-graph-nodes g12) ";\n" (d/dot-graph-edges g12) "\n}\n") (d/graph-to-dot g12)))
+      (is (= (str "digraph G {\n" (d/dot-graph-nodes g) ";\n" (d/dot-graph-edges g) "\n}\n") (d/graph-to-dot g))))))
